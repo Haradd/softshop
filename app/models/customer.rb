89b310e979a4968659d1de1fc97fa13d4e37c94f
Haradd <blazej.piaskowski@gmail.com>
2018-12-29 15:01:06 +0100

    add customer validations

--- app/models/customer.rb
+++ app/models/customer.rb
@@ -7,4 +7,9 @@ class Customer < ApplicationRecord
 
   has_many :signups, dependent: :destroy
   has_many :newsletters, through: :signups
+
+  validates :first_name, presence: true, length: { maximum: 30 }
+  validates :last_name, presence: true, length: { maximum: 30 }
+  validates :email, presence: true, length: { maximum: 30 }, uniqueness: true
+  validates :phone_number, presence: true, length: { in: 9..15 }
 end
