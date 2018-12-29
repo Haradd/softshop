0f0c8ae3408d079ab315ee4d3a93a829d6da5768
Haradd <blazej.piaskowski@gmail.com>
2018-12-29 15:15:31 +0100

    add validation to Card model

--- app/models/card.rb
+++ app/models/card.rb
@@ -5,4 +5,11 @@ class Card < ApplicationRecord
   belongs_to :customer
 
   has_many :orders, dependent: :destroy, foreign_key: :card_number, inverse_of: :card
+
+  validates :number, presence: true, uniqueness: true
+  validates :customer, presence: true
+
+  def to_s
+    self.customer.to_s + ' : ' + self.number
+  end
 end
