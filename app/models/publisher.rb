7191c66d70d98106b0a80db3829e760fdd2af53f
Haradd <blazej.piaskowski@gmail.com>
2018-12-30 22:39:40 +0100

    add validations to Publisher

--- app/models/publisher.rb
+++ app/models/publisher.rb
@@ -4,4 +4,11 @@ class Publisher < ApplicationRecord
   self.primary_key = :name
 
   has_many :products, foreign_key: :publisher_name, inverse_of: :publisher, dependent: :destroy
+
+  validates :name, presence: true, uniqueness: true, length: { maximum: 100 }
+  validates :description, length: { maximum: 300 }
+
+  def to_s
+    name
+  end
 end
