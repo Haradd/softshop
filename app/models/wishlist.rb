7cb17a84270cb521031b1a60dbdcd0326c0c5a8b
Haradd <blazej.piaskowski@gmail.com>
2018-12-30 22:57:01 +0100

    add Wishlist validations

--- app/models/wishlist.rb
+++ app/models/wishlist.rb
@@ -5,4 +5,7 @@ class Wishlist < ApplicationRecord
 
   has_many :wishlists_products, dependent: :destroy
   has_many :products, through: :wishlists_products
+
+  validates :name, presence: true
+  validates :customer, presence: true
 end
