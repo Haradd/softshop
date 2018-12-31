fb6499a00d6a7b078c888a482dfe2da4eee9ab26
Haradd <blazej.piaskowski@gmail.com>
2018-12-31 17:31:09 +0100

    add Newsletter validations

--- app/models/newsletter.rb
+++ app/models/newsletter.rb
@@ -6,4 +6,11 @@ class Newsletter < ApplicationRecord
 
   has_many :signups, dependent: :destroy
   has_many :customers, through: :signups
+
+  validates :title, presence: true, length: { maximum: 200 }
+  validates :product_type, presence: true
+
+  def to_s
+    title
+  end
 end
