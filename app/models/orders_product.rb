36b800828f32eb4602afdb8ea2dbdaee41a91dc5
Haradd <blazej.piaskowski@gmail.com>
2018-12-31 17:28:29 +0100

    add OrdersProduct valdiations

--- app/models/orders_product.rb
+++ app/models/orders_product.rb
@@ -5,4 +5,14 @@ class OrdersProduct < ApplicationRecord
 
   belongs_to :order
   belongs_to :product
+
+  validates :amount, presence: true,
+                     numericality: { only_integer: true, greater_than: 0, less_than: 9_999_999_999_999.99 }
+  validates :price, presence: true, numericality: { greater_than: 0, less_than: 9_999_999_999_999.99 }
+  validates :order, presence: true
+  validates :product, presence: true
+
+  def to_s
+    'order_id: ' + order_id.to_s + ' product_id: ' + product_id.to_s
+  end
 end
