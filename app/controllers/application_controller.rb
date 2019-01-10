e7e8d534134197c2f4a70e4b0a98ea54084cb528
Haradd <blazej.piaskowski@gmail.com>
2019-01-10 13:53:47 +0100

    handle ransack search error

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,4 +1,13 @@
 # frozen_string_literal: true
 
 class ApplicationController < ActionController::Base
+
+  rescue_from ActiveRecord::StatementInvalid, with: :ransack_search_type_error
+
+  def ransack_search_type_error
+    path = request.fullpath
+    path = path.slice(0...(path.index('?')))
+    flash[:search_error] = 'Inappropriate searching type for this column'
+    redirect_to path
+  end
 end
