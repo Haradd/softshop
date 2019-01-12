305208dd1e9e8b1d0b2960bc0dc796a0b188115f
Haradd <blazej.piaskowski@gmail.com>
2019-01-12 02:20:51 +0100

    fix resque ransack error

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -2,11 +2,14 @@
 
 class ApplicationController < ActionController::Base
 
-  rescue_from ActiveRecord::StatementInvalid, with: :ransack_search_type_error
+
+ rescue_from ActiveRecord::StatementInvalid, with: :ransack_search_type_error
 
   PAGE = 10
 
   def ransack_search_type_error
+    return unless params[:commit] = 'Search'
+
     path = request.fullpath
     path = path.slice(0...(path.index('?')))
     flash[:search_error] = 'Inappropriate searching type for this column'
